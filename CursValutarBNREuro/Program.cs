66644e7f802b70d40202d9a264c799dd47b8e09c
Tudor Anghelina <tudor.anghelina91@gmail.com>
2020-01-30 16:49:29 +0200

    Created Test Projects for reading into console xml from bnr

---
+++ CursValutarBNREuro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CursValutarBNREuro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlReader reader = XmlReader.Create("https://www.bnr.ro/nbrfxrates.xml");
+
+            reader.ReadToDescendant("Cube");
+
+            decimal value;
+            string key = null;
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.GetAttribute("currency") == "EUR")
+                    {
+                        Console.WriteLine(reader.GetAttribute("currency"));
+                        Console.WriteLine(reader.ReadInnerXml());
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CursValutarBnrXmlTest1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CursValutarBnrXmlTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlReader reader = XmlReader.Create("https://www.bnr.ro/nbrfxrates.xml");
+
+            Dictionary<string, decimal> currencyTable = new Dictionary<string, decimal>();
+
+            reader.ReadToDescendant("Cube");
+
+            decimal value = 0;
+            string key = null;
+
+            while(reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text)
+                {
+                    value = XmlConvert.ToDecimal(reader.Value);
+                    currencyTable.Add(key, value);
+                }
+
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    key = reader.GetAttribute("currency");
+                }
+            }
+
+            foreach (var element in currencyTable)
+            {
+                Console.WriteLine($"{element.Key} {element.Value}");
+            }
+
+        }
+    }
+}
